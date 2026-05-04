ce356938471c07b1b6a0067f422c18825404cd36
Daniel Volf <dan@Daniel--MacBook-Air-2.local>
2026-05-05 01:50:13 +0200

    dependency fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "normalize.css/normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./styles.css";
 import { App } from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
